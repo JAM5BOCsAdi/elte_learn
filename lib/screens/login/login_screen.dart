699a2d4b6d1ac63b17c01ebc689db50bc75dd6e5
CsAdi <oradam86@gmail.com>
2022-11-23 15:18:08 +0100

    UIParameters -> getHeight and getWidth

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:elte_learn/configs/themes/ui_parameters.dart';
 import 'package:elte_learn/packages_barrel/packages_barrel.dart';
 
 import '../../configs/themes/app_colors.dart';
@@ -9,13 +10,19 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: getWidth,
+        height: getHeight,
         decoration: BoxDecoration(
           gradient: mainGradient(),
         ),
         child: SafeArea(
           child: Column(
             children: [
-              Image.asset("assets/images/app_splash_logo.png"),
+              Image.asset(
+                "assets/images/app_splash_logo.png",
+                height: getHeight * 0.5,
+                width: getWidth * 0.5,
+              ),
             ],
           ),
         ),
