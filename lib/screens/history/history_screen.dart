45c00fe2258a8a10f17f362981b68273d835b2da
CsAdi <oradam86@gmail.com>
2023-02-23 17:37:22 +0100

    ReadMore problem

--- lib/screens/history/history_screen.dart
+++ lib/screens/history/history_screen.dart
@@ -1,6 +1,7 @@
 import 'package:elte_learn/configs/themes/ui_parameters.dart';
 import 'package:elte_learn/packages_barrel/packages_barrel.dart';
 import 'package:timeline_tile/timeline_tile.dart';
+import 'package:readmore/readmore.dart';
 
 import '../../configs/themes/app_colors.dart';
 import '../../models/event_model.dart';
@@ -43,6 +44,9 @@ TimelineTile _timelineTile({
   required Event sekHistory,
   required int index,
 }) {
+  double width = getHeight * 0.05;
+  double height = getHeight * 0.05;
+
   if (index.isEven) {
     return TimelineTile(
       alignment: TimelineAlign.manual,
@@ -50,16 +54,16 @@ TimelineTile _timelineTile({
       isFirst: index == 0,
       isLast: index == elteSekHistory.length - 1,
       indicatorStyle: IndicatorStyle(
-        width: 40,
-        height: 40,
+        width: width,
+        height: height,
         indicator: _indicator(yearNumber: sekHistory.year),
         drawGap: true,
       ),
       beforeLineStyle: LineStyle(
-        color: Colors.white.withOpacity(0.2),
+        color: Colors.lightGreenAccent.withOpacity(0.25),
         thickness: 3,
       ),
-      endChild: _gestureDetector(
+      endChild: _readMore(
         sekHistory: sekHistory,
         paddingLeft: 0.025,
         paddingRight: 0.1,
@@ -72,16 +76,16 @@ TimelineTile _timelineTile({
       isFirst: index == 0,
       isLast: index == elteSekHistory.length - 1,
       indicatorStyle: IndicatorStyle(
-        width: 40,
-        height: 40,
+        width: width,
+        height: height,
         indicator: _indicator(yearNumber: sekHistory.year),
         drawGap: true,
       ),
       beforeLineStyle: LineStyle(
-        color: Colors.white.withOpacity(0.2),
+        color: Colors.lightGreenAccent.withOpacity(0.25),
         thickness: 3,
       ),
-      startChild: _gestureDetector(
+      startChild: _readMore(
         sekHistory: sekHistory,
         paddingLeft: 0.1,
         paddingRight: 0.025,
@@ -94,9 +98,10 @@ Container _indicator({required String yearNumber}) {
   return Container(
     decoration: BoxDecoration(
       shape: BoxShape.circle,
+      gradient: kMainGradientLight,
       border: Border.fromBorderSide(
         BorderSide(
-          color: Colors.white.withOpacity(0.2),
+          color: Colors.lightGreen.shade400,
           width: 2,
         ),
       ),
@@ -111,28 +116,25 @@ Container _indicator({required String yearNumber}) {
   );
 }
 
-GestureDetector _gestureDetector({
+Padding _readMore({
   required Event sekHistory,
   required double paddingLeft,
   required double paddingRight,
 }) {
-  return GestureDetector(
-    onTap: () => print("Tapped: ${sekHistory.year}"),
-    child: Padding(
-      padding: EdgeInsets.only(
-        left: getWidth * paddingLeft,
-        right: getWidth * paddingRight,
-        top: getHeight * 0.025,
-        bottom: getHeight * 0.025,
-      ),
-      child: Text(
-        sekHistory.description,
-        overflow: TextOverflow.ellipsis,
-        maxLines: 3,
-        style: const TextStyle(
-          color: kOnSurfaceTextColor,
-          fontSize: 18,
-        ),
+  return Padding(
+    padding: EdgeInsets.only(
+      left: getWidth * paddingLeft,
+      right: getWidth * paddingRight,
+      top: getHeight * 0.025,
+      bottom: getHeight * 0.025,
+    ),
+    child: ReadMoreText(
+      sekHistory.description,
+      // overflow: TextOverflow.ellipsis,
+      trimLines: 3,
+      style: const TextStyle(
+        color: kOnSurfaceTextColor,
+        fontSize: 18,
       ),
     ),
   );
@@ -140,10 +142,10 @@ GestureDetector _gestureDetector({
 
 TimelineDivider _timelineDivider({required int index}) {
   if (index < elteSekHistory.length - 1) {
-    return const TimelineDivider(
+    return TimelineDivider(
       begin: 0.1,
       end: 0.9,
-      color: Colors.blue,
+      color: Colors.lightGreenAccent.withOpacity(0.25),
       thickness: 3,
     );
   } else {
