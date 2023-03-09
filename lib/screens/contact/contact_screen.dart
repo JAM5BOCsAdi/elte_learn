18b2afa571c504cadb228d7bfea5d70c9493ba9f
CsAdi <oradam86@gmail.com>
2023-03-09 13:11:16 +0100

    FlutterMap Curve the Edges

--- lib/screens/contact/contact_screen.dart
+++ lib/screens/contact/contact_screen.dart
@@ -34,78 +34,81 @@ class ContactScreen extends GetView<ContactController> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(
-                    height: getHeight * 0.5,
-                    width: double.maxFinite,
-                    child: Stack(
-                      children: [
-                        FlutterMap(
-                          mapController: controller.mapController,
-                          options: MapOptions(
-                            center: controller.getInitialPosition,
-                            zoom: 16.0,
-                            maxZoom: 18.0,
-                            // interactiveFlags: InteractiveFlag.all,
-                          ),
-                          layers: [
-                            TileLayerOptions(
-                              urlTemplate: controller.getUrlTemplate,
-                              subdomains: controller.getSubdomains,
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(getHeight * 0.025),
+                    child: SizedBox(
+                      height: getHeight * 0.5,
+                      width: double.maxFinite,
+                      child: Stack(
+                        children: [
+                          FlutterMap(
+                            mapController: controller.mapController,
+                            options: MapOptions(
+                              center: controller.getInitialPosition,
+                              zoom: 16.0,
+                              maxZoom: 18.0,
+                              // interactiveFlags: InteractiveFlag.all,
                             ),
-                            MarkerLayerOptions(
-                              markers: [
-                                Marker(
-                                  // width: getWidth * 0.05,
-                                  // height: getHeight * 0.05,
-                                  point: controller.getInitialPosition,
-                                  builder: (_) => Icon(
-                                    Icons.location_pin,
-                                    color: primaryLT,
-                                    size: getHeight * 0.05,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Positioned(
-                          bottom: 16.0,
-                          right: 16.0,
-                          child: Column(
-                            children: [
-                              ElevatedButton(
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all<Color>(primaryLightLT),
-                                  minimumSize: MaterialStateProperty.all<Size>(const Size(50, 50)),
-                                ),
-                                onPressed: () => controller.zoomIn(),
-                                child: const Icon(Icons.add),
+                            layers: [
+                              TileLayerOptions(
+                                urlTemplate: controller.getUrlTemplate,
+                                subdomains: controller.getSubdomains,
                               ),
-                              Gap(getHeight * 0.025),
-                              ElevatedButton(
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all<Color>(primaryLT),
-                                  minimumSize: MaterialStateProperty.all<Size>(const Size(50, 50)),
-                                ),
-                                onPressed: () => controller.zoomOut(),
-                                child: const Icon(Icons.remove),
+                              MarkerLayerOptions(
+                                markers: [
+                                  Marker(
+                                    // width: getWidth * 0.05,
+                                    // height: getHeight * 0.05,
+                                    point: controller.getInitialPosition,
+                                    builder: (_) => Icon(
+                                      Icons.location_pin,
+                                      color: primaryLT,
+                                      size: getHeight * 0.05,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
                           ),
-                        ),
-                        Positioned(
-                          bottom: 10.0,
-                          left: 16.0,
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue.shade400),
-                              // minimumSize: MaterialStateProperty.all<Size>(const Size(20, 40)),
+                          Positioned(
+                            bottom: 16.0,
+                            right: 16.0,
+                            child: Column(
+                              children: [
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all<Color>(primaryLightLT),
+                                    minimumSize: MaterialStateProperty.all<Size>(const Size(50, 50)),
+                                  ),
+                                  onPressed: () => controller.zoomIn(),
+                                  child: const Icon(Icons.add),
+                                ),
+                                Gap(getHeight * 0.025),
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all<Color>(primaryLT),
+                                    minimumSize: MaterialStateProperty.all<Size>(const Size(50, 50)),
+                                  ),
+                                  onPressed: () => controller.zoomOut(),
+                                  child: const Icon(Icons.remove),
+                                ),
+                              ],
                             ),
-                            onPressed: controller.resetMap,
-                            child: const AutoSizeText('Visszaállítás'),
                           ),
-                        ),
-                      ],
+                          Positioned(
+                            bottom: 10.0,
+                            left: 16.0,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue.shade400),
+                                // minimumSize: MaterialStateProperty.all<Size>(const Size(20, 40)),
+                              ),
+                              onPressed: controller.resetMap,
+                              child: const AutoSizeText('Visszaállítás'),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
