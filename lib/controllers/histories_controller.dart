6e138977db293bca48e71ddcb805901ff1399229
CsAdi <oradam86@gmail.com>
2023-04-06 15:26:10 +0200

    Upgrade the histories_controller

--- lib/controllers/histories_controller.dart
+++ lib/controllers/histories_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:elte_learn/packages_barrel/packages_barrel.dart';
 import 'package:elte_learn/utils/consts.dart';
 
@@ -7,23 +9,18 @@ class HistoriesController extends GetxController {
   final RxList<HistoriesModel> elteHistories = RxList([]);
   final RxList<HistoriesModel> elteSekHistories = RxList([]);
   final RxList<HistoriesModel> elteSekPtiHistories = RxList([]);
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  //   loadHistories();
-  // }
 
   Future<void> loadElteHistories() async {
     elteHistories.clear();
-    final data = await rootBundle.loadString(elteHistoryTxt);
-    final lines = data.split('\n');
+    String data = await rootBundle.loadString(elteHistoryTxt);
+    List<String> lines = data.split('§');
     print("loadElteHistories: $data");
 
     for (var line in lines) {
-      final parts = line.split(':');
+      List<String> parts = line.split(':').map((e) => e.trim()).toList();
       if (parts.length == 2) {
-        final year = parts[0];
-        final description = parts[1];
+        String year = parts[0];
+        String description = parts[1];
         elteHistories.add(HistoriesModel(year: year, description: description));
       }
     }
@@ -31,15 +28,15 @@ class HistoriesController extends GetxController {
 
   Future<void> loadElteSekHistories() async {
     elteSekHistories.clear();
-    final data = await rootBundle.loadString(elteSekHistoryTxt);
-    final lines = data.split('\n');
+    String data = await rootBundle.loadString(elteSekHistoryTxt);
+    List<String> lines = data.split('§');
     print("loadElteSekHistories: $data");
 
     for (var line in lines) {
-      final parts = line.split(':');
+      List<String> parts = line.split(':').map((e) => e.trim()).toList();
       if (parts.length == 2) {
-        final year = parts[0];
-        final description = parts[1];
+        String year = parts[0];
+        String description = parts[1];
         elteSekHistories.add(HistoriesModel(year: year, description: description));
       }
     }
@@ -47,15 +44,15 @@ class HistoriesController extends GetxController {
 
   Future<void> loadElteSekPtiHistories() async {
     elteSekPtiHistories.clear();
-    final data = await rootBundle.loadString(elteSekPtiHistoryTxt);
-    final lines = data.split('\n');
+    String data = await rootBundle.loadString(elteSekPtiHistoryTxt);
+    List<String> lines = data.split('§');
     print("loadElteSekPtiHistories: $data");
 
     for (var line in lines) {
-      final parts = line.split(':');
+      List<String> parts = line.split(':').map((e) => e.trim()).toList();
       if (parts.length == 2) {
-        final year = parts[0];
-        final description = parts[1];
+        String year = parts[0];
+        String description = parts[1];
         elteSekPtiHistories.add(HistoriesModel(year: year, description: description));
       }
     }
