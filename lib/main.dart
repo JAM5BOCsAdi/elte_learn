5bf42f828d9d66b84de4ca6b3fac2adfb66c28c6
CsAdi <oradam86@gmail.com>
2023-04-11 10:40:57 +0200

    Changed back to lightTheme

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: Get.find<ThemeController>().darkTheme,
+      theme: Get.find<ThemeController>().lightTheme,
       getPages: AppRoutes.routes(),
     );
   }
