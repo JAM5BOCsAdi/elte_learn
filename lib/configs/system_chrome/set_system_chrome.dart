2865493db8432362662817fa2eba744aa9fa5863
CsAdi <oradam86@gmail.com>
2023-04-14 09:25:49 +0200

    systemNavigationBarColor set back to transparent

--- lib/configs/system_chrome/set_system_chrome.dart
+++ lib/configs/system_chrome/set_system_chrome.dart
@@ -21,8 +21,7 @@ class SetSystemChrome extends GetxController {
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
-        // systemNavigationBarColor: Colors.black.withOpacity(0.0),
-        // systemNavigationBarColor: Colors.white, // Colors.black.withOpacity(0.002)
+        systemNavigationBarColor: Colors.transparent,
       ),
     );
   }
